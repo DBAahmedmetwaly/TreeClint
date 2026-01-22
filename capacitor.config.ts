2ffce78255db5c095d1b0c4f736e16aab616c87f
Ahmed Metwaly <luisarabss@gmail.com>
2026-01-22 10:09:55 +0000

    الإتصال اصبح لا يعمل بعد build نسخة  APK انا لا اريدها تفتح من خلال سير

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,9 @@ const config: CapacitorConfig = {
   appId: 'com.trey.clientdatamanager',
   appName: 'إدخال بيانات عملاء تري',
   server: {
-    url: 'http://localhost:9002',
+    // هام: قم بتغيير عنوان IP هذا إلى عنوان IP الخاص بالكمبيوتر
+    // الذي سيقوم بتشغيل خادم التطبيق على شبكتك.
+    url: 'http://192.168.1.100:9002',
     cleartext: true,
   }
 };
